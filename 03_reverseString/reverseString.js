53b38c929027678172a2f91c41f224413475f721
Rislowe <stonmailing@gmail.com>
2022-01-04 14:15:47 -0500

    Completed Exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
 const reverseString = function(str) {
 
+    if(str == '')
+    {
+        return '';
+    }
+
+    let reverseStr = "";
+
+    for (let i = str.length; i > 0; i--)
+    {
+        reverseStr += str.charAt(i-1);
+    }
+
+    return reverseStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
 
+    let args = Array.prototype.slice.call(arguments);
+
+    for (let i = 0; i < array.length; i++)
+    {
+        for(let j = 1; j < args.length; j++)
+        {
+            if(array[i] === args[j])
+        {
+            array.splice(i, 1);
+            i--;
+        }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
