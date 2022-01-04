9a0bf0e80ee740d019682cf1092a964876dfcd93
Rislowe <stonmailing@gmail.com>
2022-01-04 15:09:07 -0500

    Exercise Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof num1 != 'number' || typeof num2 != 'number')
+    {
+        return 'ERROR';
+    }
 
+    if(num1 < 0 || num2 < 0)
+    {
+        return 'ERROR';
+    }
+    
+    let total = 0;
+    let greaterNum, lesserNum;
+    
+    if(num1 < num2)
+    {
+        lesserNum = num1;
+        greaterNum = num2;
+    }
+    else
+    {
+        lesserNum = num2;
+        greaterNum = num1;
+    }
+
+    for(lesserNum; lesserNum <= greaterNum; lesserNum++)
+    {
+        total += lesserNum;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
+const leapYears = function(num) {
+    if(num % 4 == 0)
+    {
+        if (num < 100)
+        {
+            return true;
+        }
+        
+        if(num % 100 > 0)
+        {
+            return true;
+        }
+        
+        if(num % 100 == 0 && num % 400 == 0)
+        {
+            return true;
+        }
+    }
 
+    return false;
 };
 
 // Do not edit below this line
