4e7a711561ea449ea989d07920388357a6a7cf0b
Rislowe <stonmailing@gmail.com>
2022-01-04 12:36:34 -0500

    unskipped all tests, added Error/empty handling

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
 const repeatString = function(string, num) {
+
+    if(num < 0)
+    {
+        return 'ERROR';
+    }
+
+    if(string == '')
+    {
+        return '';
+    }
+
     let newString = "";
     for (let i = 0; i < num; i++)
     {
