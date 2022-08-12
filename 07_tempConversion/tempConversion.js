77c455635bf4c0dd0ef7a720f048e8c259942531
Rislowe <stonmailing@gmail.com>
2022-08-12 18:00:54 -0400

    completed Calculator assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celcius = (fahrenheit-32) * 5/9;
+  return parseFloat(celcius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let fahrenheit = (celcius * 9/5) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1+number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1-number2;
 };
 
 const sum = function() {
+  
+  if(arguments.length == 0) return 0;
+
+  let sum = 0;
+  
+  for(let i=0; i < arguments.length; i++)
+  {
+    if(arguments[i].length == 0) return 0;
+
+    arguments[i].forEach(element => {
+      sum = add(sum, element);      
+    });
+  };
+
+  return sum;
 	
 };
 
 const multiply = function() {
 
+  if(arguments.length == 0) return 0;
+
+  let product = 1;
+
+  for(let i=0; i < arguments.length; i++)
+  {
+    if(arguments[i].length == 0) return 0;
+
+    arguments[i].forEach(element =>{
+      product *= element;
+    });
+  };
+
+  return product;
+
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+  if(number2 == 0) return 1;
+  if(number2 == 1) return number1;
+  return number1*power(number1, number2-1);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number == 0) return 1;
+  if(number == 1) return 1;
+  return number*factorial(number-1);
 };
 
 // Do not edit below this line
