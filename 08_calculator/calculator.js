8f409ff2408877057ce4fc68d7ad5a4c87237fb3
Rislowe <stonmailing@gmail.com>
2022-08-12 19:49:24 -0400

    Finished palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,48 +6,17 @@ const subtract = function(number1, number2) {
 	return number1-number2;
 };
 
-const sum = function() {
-  
-  if(arguments.length == 0) return 0;
-
-  let sum = 0;
-  
-  for(let i=0; i < arguments.length; i++)
-  {
-    if(arguments[i].length == 0) return 0;
-
-    arguments[i].forEach(element => {
-      sum = add(sum, element);      
-    });
-  };
-
-  return sum;
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total+current, 0);
 };
 
-const multiply = function() {
-
-  if(arguments.length == 0) return 0;
-
-  let product = 1;
-
-  for(let i=0; i < arguments.length; i++)
-  {
-    if(arguments[i].length == 0) return 0;
-
-    arguments[i].forEach(element =>{
-      product *= element;
-    });
-  };
-
-  return product;
-
+const multiply = function(array) {
+  return array.length ?
+    array.reduce((product, current) => product * current, 1) : 0;
 };
 
 const power = function(number1, number2) {
-  if(number2 == 0) return 1;
-  if(number2 == 1) return number1;
-  return number1*power(number1, number2-1);
+  return Math.pow(number1, number2);
 };
 
 const factorial = function(number) {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let palindrome = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    return (
+        palindrome.split('')
+        .reverse()
+        .join('') == palindrome
+    );
 };
 
 // Do not edit below this line
