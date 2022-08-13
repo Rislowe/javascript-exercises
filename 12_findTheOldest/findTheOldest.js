ba3b369dc79a15fb15133f99ca45aa065f2bfa2b
Rislowe <stonmailing@gmail.com>
2022-08-12 20:33:51 -0400

    finsished findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.reduce((a, b) => {
 
+        const aYears = getAge(a.yearOfBirth, a.yearOfDeath);
+        const byears = getAge(b.yearOfBirth, b.yearOfDeath);
+        return aYears > byears ? a : b;
+    })
+
+    return oldest;
 };
+
+const getAge = function(birth, death)
+{
+    if (!death) death = new Date().getFullYear();
+
+    return death-birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
